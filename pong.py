abe0bcf09cf633ee3f05fcc608c611f76fa58beb
RamiroCovian <ramiro.covian@gmail.com>
2023-08-31 17:29:06 -0300

    feat: descubrimos __main__

--- pong.py
+++ pong.py
@@ -1,22 +1,34 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
+ANCHO = 800
+ALTO = 600
 
-exit = False
-while not exit:
-    # bucle principal
 
-    for event in pygame.event.get():  # Lista de eventos con objeto
-        print("Se ha producido un evento de tipo", event.type)
-        if event.type == pygame.QUIT:  # QUIT es una constante de pygame
-            print("Se ha cerrado la ventana")
-            exit = True
+class Pong:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((ANCHO, ALTO))
 
-    # Renderizar nuestro objeto
-    rectangulo = pygame.Rect(50, 100, 300, 150)
-    pygame.draw.rect(screen, (255, 255, 255), rectangulo)
-    # Mostrar los cambios en la pantalla
-    pygame.display.flip()
+    def jugar(self):  # Contiene el bucle principal
+        exit = False
+        while not exit:
+            for event in pygame.event.get():  # Lista de eventos con objeto
+                if event.type == pygame.QUIT:  # QUIT es una constante de pygame
+                    exit = True
 
-pygame.quit()
+            # Renderizar nuestro objeto
+
+            # Mostrar los cambios en la pantalla
+            pygame.display.flip()
+
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    print("Has llamado a pong.py directamente desde la linea de comandos")
+    print("El nombre del paquete ahora es", __name__)
+    juego = Pong()
+    juego.jugar()
+else:
+    print("Has llamado a pong.py desde la sentencia import")
+    print("El nombre del paquete ahora es", __name__)
