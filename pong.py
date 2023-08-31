913d1ef8a306a01c7d16191170201f26d5f49b07
RamiroCovian <ramiro.covian@gmail.com>
2023-08-31 16:35:22 -0300

    feat: toma de contacto con pygame

--- pong.py
+++ pong.py
@@ -2,3 +2,21 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+
+exit = False
+while not exit:
+    # bucle principal
+
+    for event in pygame.event.get():  # Lista de eventos con objeto
+        print("Se ha producido un evento de tipo", event.type)
+        if event.type == pygame.QUIT:  # QUIT es una constante de pygame
+            print("Se ha cerrado la ventana")
+            exit = True
+
+    # Renderizar nuestro objeto
+    rectangulo = pygame.Rect(50, 100, 300, 150)
+    pygame.draw.rect(screen, (255, 255, 255), rectangulo)
+    # Mostrar los cambios en la pantalla
+    pygame.display.flip()
+
+pygame.quit()
