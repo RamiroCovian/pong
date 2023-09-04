25b725ecdbb3ed3b104d8096b9b68fd4c7b42c6d
RamiroCovian <ramiro.covian@gmail.com>
2023-09-04 16:22:32 -0300

    fix: movemos jugadores, pelota a class

--- pong.py
+++ pong.py
@@ -7,9 +7,32 @@ COLOR_FONDO = (100, 100, 100)
 ANCHO_PALA = 20
 ALTO_PALA = 60
 MARGEN_X = 10
-COLOR_PELOTA = (239, 242, 29)
-ANCHO_PELOTA = 50
-ALTO_PELOTA = 50
+TAM_PELOTA = 20
+
+
+class Jugador:
+    def __init__(self, x, y):
+        self.rectangulo = pygame.Rect(x, y, ANCHO_PALA, ALTO_PALA)
+
+    def pintame(self, pantalla):
+        pygame.draw.rect(pantalla, COLOR_OBJETOS, self.rectangulo)
+
+
+class Pelota:
+    def __init__(self):
+        # Definicion del rectangulo
+        self.rectangulo = pygame.Rect(
+            (
+                ((ANCHO - TAM_PELOTA) / 2),
+                ((ALTO - TAM_PELOTA) / 2),
+                TAM_PELOTA,
+                TAM_PELOTA,
+            )
+        )
+
+    def pintame(self, pantalla):
+        # Pintar el rectangulo
+        pygame.draw.rect(pantalla, COLOR_OBJETOS, self.rectangulo)
 
 
 class Pong:
@@ -19,12 +42,12 @@ class Pong:
         self.title = pygame.display.set_caption("Pong")
         self.icon_imagen = pygame.image.load("icono.png")
         self.logo = pygame.display.set_icon(self.icon_imagen)
+        pos_y = (ALTO - ALTO_PALA) / 2
+        self.pelota = Pelota()
+        self.jugador1 = Jugador(MARGEN_X, pos_y)
+        self.jugador2 = Jugador(ANCHO - MARGEN_X - ANCHO_PALA, pos_y)
 
     def jugar(self):  # Contiene el bucle principal
-        pos_alto_inicial = (ALTO / 2) - (ALTO_PALA / 2)
-        centro_vectores_pelota = (ANCHO / 2, ALTO / 2)
-        radio_pelota = 10
-        ancho_linea_media = 5
         punto_jugador1 = 0
         punto_jugador2 = 0
         fuente_marcador = pygame.font.SysFont("Arial", 40)
@@ -38,27 +61,10 @@ class Pong:
             # Renderizar nuestro objeto
             pygame.draw.rect(self.screen, COLOR_FONDO, ((0, 0), (ANCHO, ALTO)))
 
-            jugador1 = pygame.Rect(MARGEN_X, pos_alto_inicial, ANCHO_PALA, ALTO_PALA)
-            pygame.draw.rect(self.screen, COLOR_OBJETOS, jugador1)
-
-            jugador2 = pygame.Rect(
-                ANCHO - MARGEN_X - ANCHO_PALA, pos_alto_inicial, ANCHO_PALA, ALTO_PALA
-            )
-            pygame.draw.rect(self.screen, COLOR_OBJETOS, jugador2)
-
-            # Linea media
-            pygame.draw.line(
-                self.screen,
-                COLOR_OBJETOS,
-                (ANCHO / 2, 0),
-                (ANCHO / 2, ALTO),
-                ancho_linea_media,
-            )
-
-            # Pelota
-            pygame.draw.circle(
-                self.screen, COLOR_PELOTA, centro_vectores_pelota, radio_pelota
-            )
+            self.pintar_red()
+            self.jugador1.pintame(self.screen)
+            self.jugador2.pintame(self.screen)
+            self.pelota.pintame(self.screen)
 
             # Marcador
             texto_marcador1 = fuente_marcador.render(
@@ -74,6 +80,21 @@ class Pong:
             pygame.display.flip()
 
         pygame.quit()
+
+    def pintar_red(self):
+        tramo_pintado = 40
+        tramo_vacio = 10
+        ancho_linea_media = 5
+        posicion_x = ANCHO / 2
+        # Linea media
+        for y in range(0, ALTO, tramo_pintado + tramo_vacio):
+            pygame.draw.line(
+                self.screen,
+                COLOR_OBJETOS,
+                (posicion_x, y),
+                (posicion_x, y + tramo_pintado),
+                ancho_linea_media,
+            )
 
 
 if __name__ == "__main__":
